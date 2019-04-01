d5bd26694bf70eeb0e1e90b2079df981d912b6ad
wulinze <824902344@qq.com>
2019-04-01 22:58:20 +0800

    新的文件结构

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,36 +1,24 @@
-//创建cookie
-function setCookie(name,value) 
-{ 
-    var Days = 30; 
-    var exp = new Date(); 
-    exp.setTime(exp.getTime() + Days*24*60*60*1000); 
-    document.cookie = name + "="+ escape (value) + ";expires=" + exp.toGMTString(); 
-} 
-//删除cookie
-function delCookie(name) 
-{ 
-    var exp = new Date(); 
-    exp.setTime(exp.getTime() - 1); 
-    var cval=getCookie(name); 
-    if(cval!=null) 
-        document.cookie= name + "="+cval+";expires="+exp.toGMTString(); 
-} 
-//读取cookie
-function getCookie(name) 
-{ 
-    var arr,reg=new RegExp("(^| )"+name+"=([^;]*)(;|$)");
- 
-    if(arr=document.cookie.match(reg))
-        return unescape(arr[2]); 
-    else 
-        return null; 
-} 
-//登录login
-function login(username,pass){
-	if(getCookie(username) != null){
-		return true;
-	}else{
-		setCookie(username,pass);
-		return true;
-	}
+
+function set_cookie(cookies){
+    document.cookie = "cookie=" + cookies;
+}
+
+function delcookie() {
+    let day = new Date();
+    day.setDate(day.getDate()-100);
+    document.cookie = "cookie=a;" + "expire=" +day;
+}
+
+function check_cookie() {
+    let exist = document.cookie.indexOf("cookie=");
+    return !(exist===-1);
+}
+
+function login() {
+    if(check_cookie()){
+        alert("welcome");
+    }else {
+        set_cookie("login");
+        alert("first");
+    }
 }
